19befe05c6c0da9477573370b91c990ea63fb09d
Tatyana Nemchina <23angorka07@gmail.com>
2025-03-17 19:18:32 +0300

    refactor: removed unnecesary JSDoc comments

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -4,7 +4,7 @@
  * @param {string} word - The word to capitalize.
  * @returns {string} - The word with its first letter capitalized.
  */
-function capitalizeFirstLetter(word: string) {
+function capitalizeFirstLetter(word: string): string {
   return word.slice(0, 1).toUpperCase() + word.slice(1);
 }
 
@@ -27,21 +27,9 @@ function addPluralEnding(itemsCount: number): string {
  * @returns {string} The formatted date as a string.
  */
 function formatDate(date: Date): string {
-  /**
-   * Gets the full English name of the month
-   * (e.g., 'January', 'February', etc.)
-   */
   const month = date.toLocaleString('en-US', { month: 'long' });
-
-  /**
-   * Obtains the full four-digit year (e.g., 2023)
-   */
   const year = date.getFullYear();
 
-  /**
-   * Constructs the final string in the format "Month Year"
-   * and returns the result.
-   */
   return `${month} ${year}`;
 }
 
