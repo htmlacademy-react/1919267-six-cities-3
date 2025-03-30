ae5348b080741139552299065a3b2d0154d088cb
Tatyana Nemchina <23angorka07@gmail.com>
2025-03-30 18:24:46 +0300

    feat: added token support

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { getToken } from './token';
 
 const BACKEND_API = 'https://15.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
@@ -7,6 +8,16 @@ export const createApi = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BACKEND_API,
     timeout: REQUEST_TIMEOUT,
+  });
+
+  api.interceptors.request.use((config) => {
+    const token = getToken();
+
+    if (token && config.headers) {
+      config.headers['x-token'] = token;
+    }
+
+    return config;
   });
 
   return api;
